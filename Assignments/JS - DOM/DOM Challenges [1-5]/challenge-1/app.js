b7325330521489faca93faa52d3277564118cf82
Nehal Adil <mdnehaladil@gmail.com>
2025-04-24 16:23:54 +0530

    challenge-1

--- Assignments/JS - DOM/DOM Challenges [1-5]/challenge-1/app.js
+++ Assignments/JS - DOM/DOM Challenges [1-5]/challenge-1/app.js
@@ -1,3 +1,25 @@
-/**
- * Write your challenge solution here
- */
+const toggleButton = document.querySelector("#toggleButton");
+const bulbStatus = document.querySelector("#status");
+const bulb = document.querySelector("#bulb");
+const body = document.querySelector("#body");
+
+let flag = false;
+
+const toggleButtonHandler = () => {
+  console.log("toggle button cliked");
+  flag = !flag;
+  console.log(flag);
+  if (flag) {
+    bulb.classList.remove("off");
+    body.classList.remove("dark-mode");
+    toggleButton.textContent = "Turn Off";
+    bulbStatus.textContent = "Status: On";
+  } else {
+    bulb.classList.add("off");
+    body.classList.add("dark-mode");
+    toggleButton.textContent = "Turn On";
+    bulbStatus.textContent = "Status: Off";
+  }
+};
+
+toggleButton.addEventListener("click", toggleButtonHandler);
