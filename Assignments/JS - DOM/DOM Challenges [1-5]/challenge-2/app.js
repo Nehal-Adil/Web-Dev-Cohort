0c073d44d7efe20eb97b5de4cdcba2bf2ca323f4
Nehal Adil <mdnehaladil@gmail.com>
2025-04-24 16:45:05 +0530

    challenge-2

--- Assignments/JS - DOM/DOM Challenges [1-5]/challenge-2/app.js
+++ Assignments/JS - DOM/DOM Challenges [1-5]/challenge-2/app.js
@@ -1,3 +1,32 @@
-/**
- * Write your challenge solution here
- */
+const redBtn = document.querySelector("#redButton");
+const greenBtn = document.querySelector("#greenButton");
+const blueBtn = document.querySelector("#blueButton");
+const purpleBtn = document.querySelector("#purpleButton");
+const resetBtn = document.querySelector("#resetButton");
+const mainHeading = document.querySelector("#mainHeading");
+const author = document.querySelector("#author");
+
+redBtn.addEventListener("click", () => {
+  mainHeading.style.color = "#f00";
+  author.style.color = "#f00";
+});
+
+greenBtn.addEventListener("click", () => {
+  mainHeading.style.color = "#0f0";
+  author.style.color = "#0f0";
+});
+
+blueBtn.addEventListener("click", () => {
+  mainHeading.style.color = "#00f";
+  author.style.color = "#00f";
+});
+
+purpleBtn.addEventListener("click", () => {
+  mainHeading.style.color = "#9b59b6";
+  author.style.color = "#9b59b6";
+});
+
+resetBtn.addEventListener("click", () => {
+  mainHeading.style.color = "#000";
+  author.style.color = "#000";
+});
