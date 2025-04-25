261827716efd18d9317fd44fa0c834725a690179
Nehal Adil <mdnehaladil@gmail.com>
2025-04-25 11:02:29 +0530

    challenge-3

--- Assignments/JS - DOM/DOM Challenges [1-5]/challenge-3/app.js
+++ Assignments/JS - DOM/DOM Challenges [1-5]/challenge-3/app.js
@@ -1,3 +1,24 @@
-/**
- * Write your challenge solution here
- */
+const nameInput = document.querySelector("#nameInput");
+const nameDisplay = document.querySelector("#nameDisplay");
+const jobInput = document.querySelector("#jobInput");
+const jobDisplay = document.querySelector("#jobDisplay");
+const ageInput = document.querySelector("#ageInput");
+const ageDisplay = document.querySelector("#ageDisplay");
+const bioInput = document.querySelector("#bioInput");
+const bioDisplay = document.querySelector("#bioDisplay");
+
+nameInput.addEventListener("input", () => {
+  nameDisplay.textContent = nameInput.value || "Not Provided";
+});
+
+jobInput.addEventListener("input", () => {
+  jobDisplay.textContent = jobInput.value || "Not Provided";
+});
+
+ageInput.addEventListener("input", () => {
+  ageDisplay.textContent = ageInput.value || "Not Provided";
+});
+
+bioInput.addEventListener("input", () => {
+  bioDisplay.textContent = bioInput.value || "Not Provided";
+});
