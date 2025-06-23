154c87dfbdea157baebb8e7fa3849790c01c8f75
Nehal Adil <mdnehaladil@gmail.com>
2025-06-23 20:58:30 +0530

    challenge-7

--- Assignments/JS - DOM/DOM Challenges [6-10]/challenge-7/app.js
+++ Assignments/JS - DOM/DOM Challenges [6-10]/challenge-7/app.js
@@ -1,3 +1,16 @@
-/**
- * Write your challenge solution here
- */
+const allAccordionBtns = document.querySelectorAll(".accordion-button");
+const allAccordionItems = document.querySelectorAll(".accordion-item");
+
+allAccordionBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const current = btn.parentElement;
+
+    const isActive = current.classList.contains("active");
+
+    allAccordionItems.forEach((item) => item.classList.remove("active"));
+
+    if (!isActive) {
+      current.classList.add("active");
+    }
+  });
+});
